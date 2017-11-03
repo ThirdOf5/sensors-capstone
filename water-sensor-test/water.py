fbcb567a20c30b56be234524e4286b6302128416
ThirdOf5 <calebjhones@gmail.com>
2017-11-03 07:49:00 +0000

    Added a directory for testing things related to the hygrometer. Added a file and got it working to read from the hygrometer

---
+++ water-sensor-test/water.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+# set RPi pin numbering scheme
+GPIO.setmode(GPIO.BCM)
+
+# define gpio pins
+channel = 17
+led = 16
+GPIO.setup(channel, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
+
+def callback(c):
+    if GPIO.input(c):
+        print("LED off")
+        GPIO.output(16, False)
+    else:
+        print("LED on")
+        GPIO.output(16, True)
+
+# tells the Pi to watch the pin and tell us when it goes either high or low
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+
+# add an event for when the water level goes above/below a certain threshold
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    time.sleep(0.1)
+
