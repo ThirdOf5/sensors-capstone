dd6b44e26ac53b9c98ed2bd8474c4679227499ac
ThirdOf5 <calebjhones@gmail.com>
2017-11-14 20:05:52 +0000

    Added basic functionality. I didn't have hardware to test it yet

--- main.py
+++ main.py
@@ -5,6 +5,29 @@
 import RPi.GPIO as GPIO
 import time
 
-if __name__ == '__main__':
-    print("placeholder")
+# set RPi pin numbering scheme
+GPIO.setmode(GPIO.BCM)
+
+# define gpio pins
+channel = 17 # Connected to the hygrometer
+relay = 16 # Connected to the relay, which controls the solenoid
+GPIO.setup(channel, GPIO.IN)
+GPIO.setup(relay, GPIO.OUT)
+
+def callback(c):
+    if not GPIO.input(c): #FIXME should this have the not?
+        GPIO.output(relay, True)
+        time.sleep(1)
+        GPIO.output(relay, False)
+        print("{}: Relay triggered ".format(time.ctime()))
+
+# tells the Pi to watch the pin and tell us when it goes either high or low
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+
+# add an event for when the water level goes above/below a certain threshold
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    # check if we need watered once every hour while running
+    time.sleep(3600)
 
